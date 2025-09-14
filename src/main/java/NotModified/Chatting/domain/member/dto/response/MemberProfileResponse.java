4df7e71def0c0031c72794c31643264b4736347c
doyeon <doyun20445@gmail.com>
2025-09-14 16:21:00 +0900

    feat: 회원 정보 DTO 구현

---
+++ src/main/java/NotModified/Chatting/domain/member/dto/response/MemberProfileResponse.java
@@ -0,0 +1,14 @@
+package NotModified.Chatting.domain.member.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class MemberProfileResponse {
+    String nickname;
+}
