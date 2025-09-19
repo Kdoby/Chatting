9ec2650428e782053c5a13d2a063d674699d72bd
eheka78 <gemddkim22@gmail.com>
2025-09-19 16:47:12 +0900

    feat: 채팅방 리스트 출력

--- src/main/frontend/src/component/ChattingRoomList.js
+++ src/main/frontend/src/component/ChattingRoomList.js
@@ -1,12 +1,50 @@
 import ChattingRoom from "./ChattingRoom";
+import AddChattingRoom from "./AddChattingRoom";
+import api from'../api';
 
-export default function ChattingRoomList({ setLeftType }){
+import React, {useEffect, useState} from "react";
+
+export default function ChattingRoomList({ setLeftType, userInfo }){
+    const [chattingList, setChattingList] = useState([]);
+    const [showAddChattingRoom, setShowAddChattingRoom] = useState(false);
+
+    // 채팅방 조회
+    const fetchChattingList = async () => {
+        try {
+            const res = await api.get("/v1/chatroom/list");
+
+            setChattingList(res.data.data); // 결과를 상태에 저장
+            console.log(res.data.data);
+        } catch (err) {
+            console.error("검색 에러:", err);
+        }
+    };
+
+    useEffect(() => {
+        fetchChattingList();
+    },[])
+
     return (
+    <>
         <div>
+            <button style={{width : "100%"}}
+                    onClick={() => setShowAddChattingRoom(true)}
+            >
+                create chatting room
+            </button>
             <ChattingRoom setLeftType={setLeftType}/>
             <ChattingRoom setLeftType={setLeftType}/>
             <ChattingRoom setLeftType={setLeftType}/>
         </div>
+
+        {/* 조건부 렌더링 */}
+        {showAddChattingRoom && (
+            <AddChattingRoom
+                onClose={() => setShowAddChattingRoom(false)}
+                userInfo={userInfo}
+            />
+        )}
+    </>
 
     );
 }
