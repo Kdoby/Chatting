821fc9f03209de288480400f3c4a5380c186bb31
eheka78 <gemddkim22@gmail.com>
2025-09-21 00:11:49 +0900

    feat: ChattingList 화면 구현

--- src/main/frontend/src/component/ChattingRoomList.js
+++ src/main/frontend/src/component/ChattingRoomList.js
@@ -15,7 +15,6 @@ export default function ChattingRoomList({ setLeftType, userInfo }){
             const res = await api.get("/v1/chatroom/list");
 
             setChattingList(res.data.data); // 결과를 상태에 저장
-            console.log(res.data.data);
         } catch (err) {
             console.error("검색 에러:", err);
         }
@@ -33,9 +32,23 @@ export default function ChattingRoomList({ setLeftType, userInfo }){
             >
                 create chatting room
             </button>
-            <ChattingRoom setLeftType={setLeftType}/>
-            <ChattingRoom setLeftType={setLeftType}/>
-            <ChattingRoom setLeftType={setLeftType}/>
+
+            {Array.isArray(chattingList) && chattingList.length > 0 ? (
+                <div>
+                    {chattingList.map((e, idx) => (
+                        <div key={idx} className="ChattingRoom_wrapper" onClick={() => setLeftType('chatting')}>
+                            <div className="ChattingRoom_img"></div>
+                            <div className="ChattingRoom_info">
+                                <div>채팅방 이름</div>
+                                <div>마지막 메세지...</div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <div>not exist</div>
+            )}
+
         </div>
 
         {/* 조건부 렌더링 */}
