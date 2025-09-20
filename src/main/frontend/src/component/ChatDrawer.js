49bce26d87aefd8f8c75f72f5879d2b9d6b036c2
eheka78 <gemddkim22@gmail.com>
2025-09-21 01:32:05 +0900

    feat: Achive Drawer Detail UI 완성

--- src/main/frontend/src/component/ChatDrawer.js
+++ src/main/frontend/src/component/ChatDrawer.js
@@ -1,6 +1,9 @@
 import React from "react";
 
+import './AddChattingRoom.css';
+
 export default function ChatDrawer ({ onClose }) {
+
     return (
         <div className="AddChattingRoom">
             <div style={{
@@ -11,7 +14,7 @@ export default function ChatDrawer ({ onClose }) {
                 {/* 닫기 버튼 */}
                 <img
                     src="images/close.png"
-                    onClick={() => onClose()}
+                    onClick={() => onClose(false)}
                     style={{
                         width: "20px", position: "absolute", top: "20px", right: "28px",
                         cursor: "pointer",
@@ -19,7 +22,74 @@ export default function ChatDrawer ({ onClose }) {
                 />
 
                 {/* 내용 */}
-                <div>archive</div>
+                <div style={{
+                    height: "100%", maxHeight: "100%", width: "100%",
+                    textAlign: "left", boxSizing: "border-box",
+                    padding: "45px 20px 20px",
+                    display: "flex", flexDirection: "column",
+                }}>
+                    <div style={{ textAlign: "center", fontSize: "25px", fontWeight: "bold", marginBottom:"20px" }}>Archive</div>
+
+                    <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
+                                  gap: "10px", margin: "10px", overflowY: "scroll", }}>
+                        <div style={{ border: "1px solid black", textAlign: "center", padding: "13px", textAlign:"left", }}>
+                            <div style={{ aspectRatio: "1/1", width: "100%", margin: "auto  0", border: "1px solid black", }}></div>
+                            <div style={{ marginTop: "5px" }}>
+                                <div style={{ fontSize: "15px" }}>2025.09.15</div>
+                                <div style={{ fontSize: "20px", fontWeight:"bold" }}>부산 여행~</div>
+                            </div>
+                        </div>
+                        <div style={{ border: "1px solid black", textAlign: "center", padding: "13px", textAlign:"left", }}>
+                            <div style={{ aspectRatio: "1/1", width: "100%", margin: "auto  0", border: "1px solid black", }}></div>
+                            <div style={{ marginTop: "5px" }}>
+                                <div style={{ fontSize: "15px" }}>2025.09.15</div>
+                                <div style={{ fontSize: "20px", fontWeight:"bold" }}>부산 여행~</div>
+                            </div>
+                        </div>
+                        <div style={{ border: "1px solid black", textAlign: "center", padding: "13px", textAlign:"left", }}>
+                            <div style={{ aspectRatio: "1/1", width: "100%", margin: "auto  0", border: "1px solid black", }}></div>
+                            <div style={{ marginTop: "5px" }}>
+                                <div style={{ fontSize: "15px" }}>2025.09.15</div>
+                                <div style={{ fontSize: "20px", fontWeight:"bold" }}>부산 여행~</div>
+                            </div>
+                        </div>
+                        <div style={{ border: "1px solid black", textAlign: "center", padding: "13px", textAlign:"left", }}>
+                            <div style={{ aspectRatio: "1/1", width: "100%", margin: "auto  0", border: "1px solid black", }}></div>
+                            <div style={{ marginTop: "5px" }}>
+                                <div style={{ fontSize: "15px" }}>2025.09.15</div>
+                                <div style={{ fontSize: "20px", fontWeight:"bold" }}>부산 여행~</div>
+                            </div>
+                        </div>
+                        <div style={{ border: "1px solid black", textAlign: "center", padding: "13px", textAlign:"left", }}>
+                            <div style={{ aspectRatio: "1/1", width: "100%", margin: "auto  0", border: "1px solid black", }}></div>
+                            <div style={{ marginTop: "5px" }}>
+                                <div style={{ fontSize: "15px" }}>2025.09.15</div>
+                                <div style={{ fontSize: "20px", fontWeight:"bold" }}>부산 여행~</div>
+                            </div>
+                        </div>
+                        <div style={{ border: "1px solid black", textAlign: "center", padding: "13px", textAlign:"left", }}>
+                            <div style={{ aspectRatio: "1/1", width: "100%", margin: "auto  0", border: "1px solid black", }}></div>
+                            <div style={{ marginTop: "5px" }}>
+                                <div style={{ fontSize: "15px" }}>2025.09.15</div>
+                                <div style={{ fontSize: "20px", fontWeight:"bold" }}>부산 여행~</div>
+                            </div>
+                        </div>
+                        <div style={{ border: "1px solid black", textAlign: "center", padding: "13px", textAlign:"left", }}>
+                            <div style={{ aspectRatio: "1/1", width: "100%", margin: "auto  0", border: "1px solid black", }}></div>
+                            <div style={{ marginTop: "5px" }}>
+                                <div style={{ fontSize: "15px" }}>2025.09.15</div>
+                                <div style={{ fontSize: "20px", fontWeight:"bold" }}>부산 여행~</div>
+                            </div>
+                        </div>
+                        <div style={{ border: "1px solid black", textAlign: "center", padding: "13px", textAlign:"left", }}>
+                            <div style={{ aspectRatio: "1/1", width: "100%", margin: "auto  0", border: "1px solid black", }}></div>
+                            <div style={{ marginTop: "5px" }}>
+                                <div style={{ fontSize: "15px" }}>2025.09.15</div>
+                                <div style={{ fontSize: "20px", fontWeight:"bold" }}>부산 여행~</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
