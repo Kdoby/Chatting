e0557edebab21b6c35135677473985e0b9d4cd75
doyeon <doyun20445@gmail.com>
2025-09-21 16:44:27 +0900

    feat: 친구 초대 권한 체크 로직 추가 및 이미지 전송 기능 추가

--- src/main/java/NotModified/Chatting/domain/chat/service/ChatService.java
+++ src/main/java/NotModified/Chatting/domain/chat/service/ChatService.java
@@ -1,11 +1,11 @@
 package NotModified.Chatting.domain.chat.service;
 
 import NotModified.Chatting.domain.chat.dto.request.ChatRequest;
+import NotModified.Chatting.domain.chat.dto.response.ChatImageResponse;
 import NotModified.Chatting.domain.chat.dto.response.ChatResponse;
 import NotModified.Chatting.domain.chat.dto.response.ChatRoomInfoResponse;
 import NotModified.Chatting.domain.chat.dto.response.ChatRoomResponse;
-import NotModified.Chatting.domain.chat.model.ChatRoom;
-import NotModified.Chatting.domain.member.model.Member;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -16,9 +16,6 @@ public interface ChatService {
 
     /* 채팅방에 친구 초대 */
     ChatResponse inviteMembers(Long roomId, List<Long> participants, Long userId);
-
-    /* 채팅방 입장 */
-    ChatResponse enterRoom(Long roomId, Long userId);
 
     /* 채팅방 나가기 */
     ChatResponse leaveRoom(Long roomId, Long userId);
@@ -32,6 +29,12 @@ public interface ChatService {
     /* 채팅방 정보 조회 */
     ChatRoomInfoResponse getChatRoomInfo(Long roomId, Long userId);
 
-    /* 메시지 보내기 (서버가 받은 메시지를 response 로 바꿔서 브로드 캐스팅하기 위한 함수) */
-    ChatResponse sendMessage(Long roomId, Long userId, ChatRequest request);
+    /* 텍스트 메시지 보내기 (서버가 받은 메시지를 response 로 바꿔서 브로드 캐스팅하기 위한 함수) */
+    ChatResponse saveTextMessage(Long roomId, Long userId, ChatRequest request);
+
+    /* 이미지 메시지 보내기 */
+    ChatResponse saveImageMessage(Long roomId, Long userId, List<MultipartFile> files);
+
+    /* 특정 채팅방에서 보낸 이미지 목록 조회 */
+    List<ChatImageResponse> getImagesInChatRoom(Long roomId, Long userId);
 }
