6864d7e12f4933e6bed6c3b9964a24129fa5c443
eheka78 <gemddkim22@gmail.com>
2025-09-23 16:07:11 +0900

    feat: ChatDrawer에 ArchiveDetail 연결

--- src/main/frontend/src/component/ChatDrawer.js
+++ src/main/frontend/src/component/ChatDrawer.js
@@ -1,4 +1,5 @@
 import EditArchive from "./EditArchive";
+import ArchiveDetail from "../archive/ArchiveDetail";
 
 import './AddChattingRoom.css';
 import '../chatting/Chatting.css';
@@ -11,6 +12,8 @@ export default function ChatDrawer ({ roomId, onClose }) {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [modalType, setModalType] = useState(null); // 0=닫힘, 1=수정
     const [chattingRoomArchiveList, setChattingRoomArchiveList] = useState([]);
+    const [showArchiveDetail, setShowArchiveDetail] = useState(false);
+    const [archiveId, setArchiveId] = useState('');
 
     // 아카이브 삭제 -> 아직 연결 안 함
     const deleteArchive = async () => {
@@ -74,9 +77,16 @@ export default function ChatDrawer ({ roomId, onClose }) {
                             {Array.isArray(chattingRoomArchiveList) && chattingRoomArchiveList.map((e) => (
                                 <div key={e.archiveId}
                                      style={{ border: "1px solid black", textAlign: "center", padding: "13px", textAlign:"left", }}>
-                                    <div style={{ aspectRatio: "1/1", width: "100%", margin: "auto  0", border: "1px solid black", }}>
+                                    <div style={{ aspectRatio: "1/1", border: "1px solid black", display: "flex",
+                                                  justifyContent: "center", alignItems: "center", textAlign: "center",
+                                         }}
+                                         onClick={() => {
+                                            setArchiveId(e.archiveId);
+                                            setShowArchiveDetail(true);
+                                         }}
+                                    >
                                         <img src={`http://localhost:8080${e.thumbnailImage}`}
-                                             style={{width:"100%"}} />
+                                             style={{ width:"100%", height:"100%", objectFit: "contain", }} />
                                     </div>
                                     <div style={{ marginTop: "5px" }}>
                                         <div style={{ fontSize: "15px", display: "flex", alignItems: "center", position: "relative" }}>
@@ -107,6 +117,15 @@ export default function ChatDrawer ({ roomId, onClose }) {
             </div>
 
             {modalType === 1 && (<EditArchive onClose={() => setModalType(0)}/>)}
+
+            {/* 조건부 렌더링 */}
+            {showArchiveDetail && (
+                <ArchiveDetail
+                    onClose={() => setShowArchiveDetail(false)}
+                    setArchiveId={setArchiveId}
+                    archiveId={archiveId}
+                />
+            )}
         </div>
     );
 }
